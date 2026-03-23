6efacdf2babdff8393c756c5abfd0b679fb62b83
Ethan J. Alvarée <21269624+EthanAlvaree@users.noreply.github.com>
2026-03-22 19:46:57 -0700

    Revert "Mega Menu, Made Navigation Data-Driven, added Hero banner"
    This reverts commit 5b4af640803080e1e51b0c20281d4bc05d486045.

--- components/header/Dropdown.tsx
+++ components/header/Dropdown.tsx
@@ -20,29 +20,15 @@ export default function Dropdown({ title }: DropdownProps) {
       </button>
 
       {open && (
-        <div className="absolute left-0 top-10 bg-white text-black shadow-xl p-6 w-[500px] grid grid-cols-2 gap-6 z-50">
-
-          {/* Column 1 */}
-          <div>
-            <h4 className="font-semibold mb-2">About</h4>
-            <ul className="space-y-1 text-sm">
-              <li className="hover:text-blue-600">Our Approach</li>
-              <li className="hover:text-blue-600">Our Students</li>
-              <li className="hover:text-blue-600">Our Teachers</li>
-              <li className="hover:text-blue-600">Leadership</li>
-            </ul>
-          </div>
-
-          {/* Column 2 */}
-          <div>
-            <h4 className="font-semibold mb-2">More</h4>
-            <ul className="space-y-1 text-sm">
-              <li className="hover:text-blue-600">Campus</li>
-              <li className="hover:text-blue-600">History</li>
-              <li className="hover:text-blue-600">Employment</li>
-            </ul>
-          </div>
-
+        <div className="absolute top-8 bg-white text-black shadow-lg w-60 p-4">
+          <ul className="space-y-2">
+            <li className="hover:text-blue-600">Our Approach</li>
+            <li className="hover:text-blue-600">Our Students</li>
+            <li className="hover:text-blue-600">Our Teachers</li>
+            <li className="hover:text-blue-600">Leadership</li>
+            <li className="hover:text-blue-600">Campus</li>
+            <li className="hover:text-blue-600">History</li>
+          </ul>
         </div>
       )}
     </div>
